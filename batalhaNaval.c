ee45cd7e3b51067a5548c77d3f432a08fee4bf28
Lunatic Fox <joseliojrx25@gmail.com>
2025-12-28 20:06:14 -0300

    Commit nível aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // Desafio Batalha Naval - MateCheck
-// Nível: Novato
+// Nível:Aventureiro
 
 int main() {
   int board[10][10];
@@ -18,6 +18,14 @@ int main() {
   // Navio horizontal: [1, 3] [1, 4] [1, 5]
   for(int y = 3; y < 6; y++)
     board[1][y] = 3;
+
+  // Navio diagonal 1: [5, 1] [6, 2] [7, 3]
+  for(int x = 5, y = 1; x < 8, y < 4; x++, y++)
+    board[x][y] = 3;
+  
+  // Navio diagonal 2: [3, 8] [4, 7] [5, 6]
+  for(int x = 3, y = 8; x < 6, y > 5; x++, y--)
+    board[x][y] = 3;
 
   // Exibição do tabuleiro
   for(int i = 0; i < 10; i++) {
